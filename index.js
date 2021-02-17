e31a3e098ef7870b294b8a59e3dcf7c16b09dfa3
aaron-lethers <69817429+aaron-lethers@users.noreply.github.com>
2021-02-17 14:59:54 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+var drums = document.querySelectorAll(".drum");
+
+for(var i = 0; i < drums.length; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var letter = this.innerHTML;
+    playSound(letter);
+    animation(letter);
+  });
+}
+
+document.addEventListener("keydown", function(e){
+  playSound(e.key);
+  animation(e.key);
+})
+
+function animation(letter){
+  var activeButton = document.querySelector("." + letter);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
+
+
+function playSound(letter){
+  switch (letter) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    default:
+      console.log("Error");
+  }
+}
